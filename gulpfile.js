6098aea311e29eb47b83c0c9653ea5892c57a545
TommyKolcsey <tommykolcsey@yahoo.com>
2016-02-06 16:19:16 +0000

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify'),
+	connect = require('gulp-connect')
+	
+
+var jsSources = [
+	'components/scripts/*.js'
+];
+
+var sassSources = [
+	'components/sass/style.scss' 
+];
+
+var htmlSources = [
+	'builds/development/*.html'
+];
+
+var jsonSources = [
+	'builds/development/js/*.json'
+];
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
+});
+
+//gulp.task('compass', function(){
+//	gulp.src(sassSources)
+//		.pipe(compass({
+//			sass: 'components/sass',
+//			image: 'builds/development/img',
+//			style: 'expanded'
+//		}))
+//		.on('error', gutil.log)
+//		.pipe(gulp.dest('builds/development/css'))
+//		.pipe(connect.reload())
+//});
+
+gulp.task('watch', function() {
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss',['compass']); 
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('css', function(){
+	gulp.src(cssSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'css', 'js', 'watch', 'connect'])
